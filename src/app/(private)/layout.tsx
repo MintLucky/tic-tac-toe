cc7c3771bafbd10c52dfdfc9cd52f56ca25aecb0
Alex <Alexander.Bashlykov@sitewerk.ch>
2025-01-08 18:09:26 -0300

    move page and game to private folder that requires auth

---
+++ src/app/(private)/layout.tsx
@@ -0,0 +1,32 @@
+import { sessionService } from "@/entities/user/server";
+import { Button } from "@/shared/ui/button";
+import { redirect } from "next/navigation";
+
+export default async function PrivateLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { session } = await sessionService.verifySession();
+
+  return (
+    <div className="flex flex-col grow">
+      <header className="px-10 py-4 flex flex-row gap-4 justify-between border-b border-b-primary/50">
+        <div>Tik tak toe</div>
+        <div className="flex items-center gap-4">
+          <div className="text-lg">{session.login}</div>
+          <form
+            action={async () => {
+              "use server";
+              sessionService.deleteSession();
+              redirect("/sign-in");
+            }}
+          >
+            <Button>Sign out</Button>
+          </form>
+        </div>
+      </header>
+      {children}
+    </div>
+  );
+}
