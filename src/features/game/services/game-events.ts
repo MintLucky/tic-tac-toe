48b2ec4fbb73ca9c6da781f5260b529dd74f2506
Alex <Alexander.Bashlykov@sitewerk.ch>
2025-01-11 00:00:04 -0300

    add RabbitMQ and use it for events sending

--- src/features/game/services/game-events.ts
+++ src/features/game/services/game-events.ts
@@ -1,5 +1,6 @@
 import { GameDomain } from "@/entities/game"
 import { GameId } from "@/kernel/ids"
+import { EventsChanel } from "@/shared/lib/events"
 
 type GameEvent = {
   type: 'game-changed',
@@ -10,29 +11,20 @@ type Listener = (game: GameEvent) => void
 
 class GameEventService {
 
-  listeners = new Map<GameId, Set<Listener>>();
+  eventsChanel = new EventsChanel("game");
 
   addListener(gameId: GameId, listener: Listener) {
-    let listeners = this.listeners.get(gameId)
-    if (!listeners) {
-      listeners = new Set([listener])
-      this.listeners.set(gameId, listeners)
-    }
-
-    listeners.add(listener)
-
-    return () => {
-      listeners.delete(listener)
-    }
+    return this.eventsChanel.consume(gameId, (data) => {
+      listener(data as GameEvent);
+    });
   };
 
   emit(game: GameDomain.GameEntity) {
-    const listeners = this.listeners.get(game.id) ?? new Set()
-    for(const listener of listeners) {
-      listener({ type: 'game-changed', data: game })
-    }
+      return this.eventsChanel.emit(game.id, {
+        type: "game-changed",
+        data: game,
+      } satisfies GameEvent);
   }
-  
 }
 
 export const gameEvents = new GameEventService;
